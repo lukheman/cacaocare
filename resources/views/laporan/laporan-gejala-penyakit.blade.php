64a1116160b4c08d9245f0709d06a86309374b7d
Akmal <akmalthemalong@gmail.com>
2025-05-21 10:13:49 +0800

    hapus seeder

--- resources/views/laporan/laporan-gejala-penyakit.blade.php
+++ resources/views/laporan/laporan-gejala-penyakit.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laporan Barang Masuk</title>
+    <title>Laporan Gejala Penyakit</title>
 
 
     <!-- TODO: buat perhari -->
