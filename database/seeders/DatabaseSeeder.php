dba987e56577f9d803d1c8193cda2fb4939810a2
Akmal <akmalthemalong@gmail.com>
2025-06-03 10:47:02 +0800

    feat(migration): perbaiki nama tabel pada fungsi drop dari 'penyakits' menjadi 'penyakit'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        /* User::factory(10)->create(); */
-
         User::factory()->create([
             'name' => 'Akmal admin',
             'email' => 'admin@gmail.com',
