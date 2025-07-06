6db6748f7c0f7236824e7c9ea4a0c04084cf2e45
Akmal <akmalthemalong@gmail.com>
2025-07-06 08:24:51 +0800

    feat(diagnosis): Menampilkan tabel keterangan nilai persentase

--- resources/views/livewire/diagnosis/hasil-diagnosis.blade.php
+++ resources/views/livewire/diagnosis/hasil-diagnosis.blade.php
@@ -23,6 +23,38 @@
             @endforeach
         </ul>
 
+        <table class="table table-striped table-bordered mb-4">
+            <thead class="table-primary">
+                <tr>
+                    <th>No</th>
+                    <th>Keterangan</th>
+                    <th>Nilai presentase</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Kemungkinan kecil</td>
+                    <td>0% - 50%</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Kemungkinan</td>
+                    <td>51% - 79%</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Kemungkinan besar</td>
+                    <td>80% - 99%</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Sangat berkemungkinan/sangat yakin</td>
+                    <td>100%</td>
+                </tr>
+            </tbody>
+        </table>
+
         @foreach ($penyakit as $item)
             <div class="mb-4">
                 <h5 class="text-primary">Penjelasan Penyakit {{ $item->nama }}</h5>
@@ -40,4 +72,4 @@
             </button>
         </div>
     </div>
-</div>
+</div>/div>
